c56a38c44152d671e20a618adfc45dd2fa6fd32f
Marquis Abah <marquis.abah@shuttlers.ng>
2024-01-28 10:13:38 +0100

    complete authentication routes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,34 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import { getUserByEmail, createUser, UserModel } from "../models/users";
 import jwt from "jsonwebtoken";
-const JWT_SECRET="NIMELSSA_QUIZ_2024"
-// export const register = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-//    try {
-//       const { firstname, lastname, email, matric, password } = req.body
-//       if(!firstname || !lastname || !email || !matric || !password){
-//         res.status(400).json({errorMessage: 'Incomplete request data'})
-//       }
-
-//       const existingUser = await getUserByEmail(email)
-//       if(existingUser){
-//         return res.status(400).json({errorMessage: 'User already exist'})
-//       }
-
-//       const salt = random()
-
-//       const userPayload = {
-//         firstname, lastname, email, matric, authentication: {
-//             salt, password: authentication(salt, password)
-//           }
-//       }
-//       const newUser = await createUser(userPayload)
-
-//      return res.status(200).json({successMessage: 'User was successfully created', user: newUser})
-//    } catch (error) {
-//     console.log(error, 'error here')
-//      res.status(500).json({errorMessage: 'Something went wrong'})
-//    }
-// }
+// const JWT_SECRET="NIMELSSA_QUIZ_2024"
 
 export const register = async (
   req: express.Request,
@@ -72,36 +45,6 @@ export const register = async (
   }
 };
 
-// export const login = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-//   try {
-//      const { matric, password } = req.body
-//      if(!matric || !password){
-//        res.status(400).json({errorMessage: 'Incomplete request data'})
-//      }
-
-//      const user = await getUserByMatric(matric).select('+authentication.salt +authentication.password')
-//      if(!user){
-//       res.status(400).json({errorMessage: 'User does not exist'})
-//       return
-//      }
-
-//      const expectedHash = authentication(user.authentication.salt, password)
-
-//      if(user.authentication.password !== expectedHash){
-//       res.status(404).json({errorMessage: 'Invalid credentials'})
-//       return
-//      }
-
-//      const salt = random()
-//      user.authentication.sessionToken = authentication(salt, user._id.toString())
-//      await user.save()
-//      res.cookie('QUIZ-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' })
-//      return res.status(200).json(user)
-//   } catch (error) {
-//     res.status(500).json({errorMessage: 'Something went wrong'})
-//   }
-// }
-
 export const login = async (
   req: express.Request,
   res: express.Response,
@@ -134,7 +77,7 @@ export const login = async (
       id: user._id,
       role: user.role,
     };
-    const accessToken = jwt.sign(payload, JWT_SECRET, {
+    const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: maxAge,
     });
     user.authentication.sessionToken = accessToken
@@ -143,10 +86,19 @@ export const login = async (
       path: "/",
       maxAge: maxAge * 1000,
       httpOnly: true,
-      secure: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
     });
     return res.status(200).json({ user, token: accessToken });
   } catch (error) {
     res.status(500).json({ errorMessage: "Something went wrong" });
   }
+};
+
+export const logoutUser = async (req: express.Request, res: express.Response) => {
+  res.cookie("QUIZ_AUTH_TOKEN", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  return res.status(200).json({ message: "Successfully logged out" });
 };
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import dotenv from "dotenv";
+import helmet from "helmet";
 import { checkUserAccess } from './middlewares/authenticate'
 import { connectDB } from '../config/db.config'
 import authRouter from './router/authentication'
 import quizRouter from './router/quiz'
 import userRouter from './router/users'
-
+dotenv.config();
 const app = express()
 connectDB()
 
@@ -16,8 +18,10 @@ app.use(cors({
     credentials: true
 }))
 app.use(compression())
+app.use(helmet());
 app.use(cookieParser())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 app.use('/v1/auth', authRouter)
 app.use('/v1/quiz', checkUserAccess, quizRouter)
